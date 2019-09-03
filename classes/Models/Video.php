d8d8151b79c3690101f406de4c7b6e829d797691
Narayana <madabushi-l@webmail.uwinnipeg.ca>
2019-09-03 16:07:32 -0500

    Added update primary table query

--- classes/Models/Video.php
+++ classes/Models/Video.php
@@ -22,16 +22,6 @@ class Video extends Model
 
 	public $release_date;
 
-	public function __construct($title, $video_type, $language, $rating,$synopsis,$num_of_season, $release_date){
-		$this->title = $title;
-	    $this->video_type = $video_type;
-		$this->language = $language;
-		$this->rating = $rating;
-		$this->synopsis = $synopsis;
-		$this->num_of_season = $num_of_season;
-		$this->release_date = $release_date;
-	}
-
 	public function save($video)
 	{	
 		$query = "INSERT INTO {$this->table} (title, video_type, language, rating, synopsis, num_of_season, release_date) VALUES(:title, :video_type, :language, :rating, :synopsis, :num_of_season, :release_date)";
@@ -48,12 +38,32 @@ class Video extends Model
 		return static::$dbh->lastInsertId();
 	}
 		
-
-
-	
-
-	public function update()
+	public function update($data)
 	{
+		$data['release_date'] = date("Y-m-d H:i:s", strtotime($data['release_date'])); 
+		$query = 'UPDATE vid_collection
+          SET
+          title = :title,
+          video_type = :video_type,
+          language = :language,
+          rating = :rating,
+          synopsis = :synopsis,
+          num_of_season = :num_of_season,
+          release_date = :release_date
+          WHERE
+          video_id = :video_id';
 
+      	$stmt = static::$dbh->prepare($query);
+
+      	$params = array('title' => $data['title'],
+              'video_type' => $data['video_type'],
+              'language' => $data['language'],
+              'rating' => $data['rating'],
+              'synopsis' => $data['synopsis'],
+              'num_of_season' => $data['num_of_season'],
+              'release_date' => $data['release_date'],
+          	  'video_id' => $data['video_id']);
+
+        return $stmt->execute($params);
 	}
 }
